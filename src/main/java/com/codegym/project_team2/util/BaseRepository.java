ee9f106936705b11b2d876045960dae90c807efe
Lê Anh Quân <anhquan.dns@gmail.com>
2025-03-21 09:54:49 -0500

    formate code and update

--- src/main/java/com/codegym/project_team2/util/BaseRepository.java
+++ src/main/java/com/codegym/project_team2/util/BaseRepository.java
@@ -5,14 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class BaseRepository {
-    private static final String URL ="jdbc:mysql://localhost:3306/food_ordering_system";
-    private static final String USER ="root";
-    private static final String PASS ="codegym";
-    public static Connection getConnectDB(){
+    private static final String URL = "jdbc:mysql://localhost:3306/food_ordering_system";
+    private static final String USER = "root";
+    private static final String PASS = "codegym";
+
+    public static Connection getConnectDB() {
         Connection connection = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection= DriverManager.getConnection(URL,USER,PASS);
+            connection = DriverManager.getConnection(URL, USER, PASS);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException throwables) {
