4ed028e7bb0cfe1ea1dc09a380b32a4d6b933611
Kadb96 <ptvu.2838@gmail.com>
2025-03-25 03:45:24 +0700

    them tinh nang dong mo nha hang

--- src/main/java/com/codegym/project_team2/repository/IRestaurantRepository.java
+++ src/main/java/com/codegym/project_team2/repository/IRestaurantRepository.java
@@ -1,17 +1,22 @@
 package com.codegym.project_team2.repository;
 
+import com.codegym.project_team2.dto.RestaurantDto;
 import com.codegym.project_team2.model.Restaurant;
 
 import java.util.List;
 
 public interface IRestaurantRepository {
-    List<Restaurant> showAll();
+    List<RestaurantDto> showAll();
 
-    Restaurant show(String ownerId);
+    RestaurantDto show(int ownerId);
 
-    boolean delete(String ownerId);
+    boolean delete(int ownerId);
 
-    boolean update(String ownerId, Restaurant restaurant);
+    boolean update(int ownerId, Restaurant restaurant);
 
     boolean create(Restaurant restaurant);
+
+    boolean open(int ownerId);
+
+    boolean close(int ownerId);
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,5 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,5 +10,6 @@
 </h1>
 <br/>
 <a href="hello-servlet">Hello Servlet</a>
+<c:redirect url="/login"></c:redirect>
 </body>
 </html>
